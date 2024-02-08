c7f9ffb45f6f0763ea605e6372d77e74e0a98282
vanom <envinenvin@gmail.com>
2024-02-08 23:41:33 +0200

    fix: changed to hash router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { SelectedCardPage, WeatherCardsPage } from "../pages";
 
 const routes = [
@@ -7,7 +7,7 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
